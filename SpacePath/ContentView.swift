3d3e5cae38a615457fd74279c99474f023e6f967
Rayan <rayyan655@icloud.com>
2025-11-02 16:42:04 +0300

    Initial Commit

---
+++ SpacePath/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SpacePath
+//
+//  Created by Rayan Alzahrani on 11/05/1447 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SpacePath/SpacePathApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpacePathApp.swift
+//  SpacePath
+//
+//  Created by Rayan Alzahrani on 11/05/1447 AH.
+//
+
+import SwiftUI
+
+@main
+struct SpacePathApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
